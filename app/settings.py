1a71b6c2bb4825d56dc6cfcc80271bf3fda6e3b6
arjun singh tomar <arjunsinghtomar03511@gmail.com>
2024-04-25 22:50:32 +0530

    removed stock data route

--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7-ajb8qe1=*v#%+*a%7$92lem7fflh)kw)=$(r%y+lmttzfps&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get("DEBUG",0)))
 
 ALLOWED_HOSTS = ["*"]
 
