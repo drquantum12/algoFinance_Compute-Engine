782a927e65693a2614bbeb4063319b0cac9c44c4
arjun singh tomar <arjunsinghtomar03511@gmail.com>
2024-05-14 23:16:25 +0530

    rectified news route

--- finance/urls.py
+++ finance/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('login/', views.login, name="login"),
     path('adminPanel/', views.updateRecordPage, name="adminPanel"),
+    path('news/', views.news, name="news_all"),
     path('news/<news_type>/', views.news, name="news"),
     path('stock_data/get/', views.stock_data, name="stock_data"),
     path('update_records/', views.updateData, name="update_records"),
